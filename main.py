8d6cbeb54e6190d966f1ca7a1793f0de4b45e089
lilia-levern <lilia.levern@edu.esiee.fr>
2024-12-12 09:59:39 +0000

    un message explicatif

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-#### Imports et définition des variables globales
+"""jjj"""#### Imports et définition des variables globales
 
 
 #### Fonctions secondaires
 
 
 def artcode_i(s):
-    """retourne la liste de tuples encodant une chaîne de caractères passée en argument selon un algorithme itératif
+    """retourne la liste de tuples encodant une chaîne de caractères passée 
 
     Args:
         s (str): la chaîne de caractères à encoder
@@ -15,13 +15,13 @@ def artcode_i(s):
     """
     resultat=[]
     occurence=0
-    lettre_courante=None 
-   
+    lettre_courante=None
+
     for lettre in s:
-         if(lettre_courante == lettre):
+        if lettre_courante == lettre :
             occurence+=1
 
-         else:
+        else:
             resultat.append((lettre_courante, occurence))
             lettre_courante=lettre
             occurence+=1
@@ -29,7 +29,7 @@ def artcode_i(s):
 
 
 def artcode_r(s):
-    """retourne la liste de tuples encodant une chaîne de caractères passée en argument selon un algorithme récursif
+    """retourne la liste de tuples encodant une chaîne de caractères pas
 
     Args:
         s (str): la chaîne de caractères à encoder
@@ -38,23 +38,24 @@ def artcode_r(s):
         list: la liste des tuples (caractère, nombre d'occurences)
         """
     if not s :
-            return []
+        return []
 
     lettre_courante=s[0]
     occurence=1
-    for lettre in s:  
+    for lettre in s[1:]:
         if lettre==lettre_courante :
             occurence+=1
         else :
-            break    
+            break
 
-        return [(lettre_courante,occurence)]+artcode_r(s[occurence:])
-        
+    return [(lettre_courante,occurence)]+artcode_r(s[occurence:])
+
 
 #### Fonction principale
 
 
 def main():
+    """jjjjjjj"""
     print(artcode_i('MMMMaaacXolloMM'))
     print(artcode_r('MMMMaaacXolloMM'))
 
